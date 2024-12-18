b7c7b911d022994d2bac2d21bbc00f7799ac21cb
Vineethanand <vineethanand.h@gmail.com>
2024-12-19 00:56:29 +0530

    RDS config

--- src/main/java/com/product/productservice/configs/SecurityConfig.java
+++ src/main/java/com/product/productservice/configs/SecurityConfig.java
@@ -1,26 +1,26 @@
-package com.product.productservice.configs;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/products/{id}")
-                                //.hasAnyAuthority("ADMIN")
-                                //.anyRequest()
-                                //.permitAll()
-                                .authenticated()
-//                        .hasAuthority("SCOPE_ADMIN")
-                        //.anyRequest().permitAll()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-    }
-}
+//package com.product.productservice.configs;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                                .requestMatchers("/products/{id}")
+//                                //.hasAnyAuthority("ADMIN")
+//                                //.anyRequest()
+//                                //.permitAll()
+//                                .authenticated()
+////                        .hasAuthority("SCOPE_ADMIN")
+//                        //.anyRequest().permitAll()
+//                )
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+//        return http.build();
+//    }
+//}
